a0f22cf5141e6dfb935b351fb0ebc6c9557ffdb8
Zachary Boelter <zboelter@gmail.com>
2022-01-05 15:15:34 -0600

    conditionals practice

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log('hellow orld');
+
+let value = prompt('What is the official name of JavaScript?', '');
+
+if (value == "ECMAScript") {
+    alert('Right!')
+} else {
+    alert('You dumb cunt, no!')
+};
+
+
